e38b18438c88e9383457d1ef24214be19e3fbeea
Lakshya Parmar <lakshyaparmar1282@gmail.com>
2026-07-26 13:36:16 +0530

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,41 @@
 import type { Metadata } from "next";
-import { Rubik } from "next/font/google";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll";
 
-const rubik = Rubik({
-  variable: "--font-rubik",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
+  weight: ["400"],
   display: "swap",
+});
+
+const ivyMode = localFont({
+  src: [
+    {
+      path: "../../ivy-mode/IvyMode-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    }
+  ],
+  variable: "--font-ivy-mode",
+});
+
+const vazirmatn = localFont({
+  src: [
+    {
+      path: "../../Vazirmatn/static/Vazirmatn-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../Vazirmatn/static/Vazirmatn-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    }
+  ],
+  variable: "--font-vazirmatn",
 });
 
 export const metadata: Metadata = {
@@ -23,9 +51,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${rubik.variable} h-full antialiased`}
+      className={`${inter.variable} ${ivyMode.variable} ${vazirmatn.variable} h-full antialiased`}
     >
-      <body className={`${rubik.className} min-h-full flex flex-col`}>
+      <body className="font-sans min-h-full flex flex-col">
         <SmoothScroll />
         {children}
       </body>
